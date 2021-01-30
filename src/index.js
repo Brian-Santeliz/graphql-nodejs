447b623d8241f9c32346390bc15994e20e324ccf
Brian <blsvsanteliz@gmail.com>
2021-01-30 00:53:13 -0400

    create mutation to delete a book, add try catch statement

--- src/index.js
+++ src/index.js
@@ -16,29 +16,59 @@ const typeDefs = gql`
   type Mutation {
     createBook(name: String!, year: String!, author: String!): Book!
     deleteBook(id: ID!): Book
+    updateBook(name: String!, year: String!, author: String!, id: ID!): Book!
   }
 `;
 const resolvers = {
   Query: {
     getBooks: async () => {
-      return await Books.find();
+      try {
+        return await Books.find();
+      } catch (error) {
+        console.error(`Error:${error}`);
+      }
     },
     getBookId: async (_, { id }) => {
-      return await Books.findById({ _id: id });
+      try {
+        return await Books.findById({ _id: id });
+      } catch (error) {
+        console.error(`Error:${error}`);
+      }
     },
   },
   Mutation: {
     createBook: async (parent, args, context, info) => {
       const { name, year, author } = args;
-      const book = new Books({
-        name,
-        year,
-        author,
-      });
-      return await book.save();
+      try {
+        const book = new Books({
+          name,
+          year,
+          author,
+        });
+        return await book.save();
+      } catch (error) {
+        console.error(`Error:${error}`);
+      }
     },
     deleteBook: async (_, { id }) => {
-      return await Books.findByIdAndRemove({ _id: id });
+      try {
+        return await Books.findByIdAndRemove({ _id: id });
+      } catch (error) {
+        console.error(`Error:${error}`);
+      }
+    },
+    updateBook: async (_, { name, year, author, id }) => {
+      return await Books.findByIdAndUpdate(
+        { _id: id },
+        {
+          name,
+          year,
+          author,
+        },
+        {
+          new: true,
+        }
+      );
     },
   },
 };
